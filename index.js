4fff29a908e704b54c746fdceafd1f39ff7818bd
JAMES ARASIM <jaemzware@hotmail.com>
2025-03-09 20:14:48 -0700

    bug

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 //openssl genrsa -out key.pem 2048
 //openssl req -new -sha256 -key key.pem -out csr.csr
 //openssl req -x509 -sha256 -days 365 -key key.pem -in csr.csr -out certificate.pem
-const options = {
+// const options = {
     // key: fs.readFileSync('./sslcert/key.pem'),
     // cert: fs.readFileSync('./sslcert/certificate.pem')
 // };
